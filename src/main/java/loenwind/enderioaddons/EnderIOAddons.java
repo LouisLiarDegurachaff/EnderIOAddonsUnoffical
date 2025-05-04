9dc08f018acaa8345d0dcf08a7e9ee4a116615ce
Lu <akitomoya2908@gmail.com>
2025-05-04 22:54:58 +0700

    update

--- src/main/java/loenwind/enderioaddons/EnderIOAddons.java
+++ src/main/java/loenwind/enderioaddons/EnderIOAddons.java
@@ -40,7 +40,7 @@ public class EnderIOAddons implements InitAware {
     @Nonnull
     public static final String MOD_NAME = "Ender IO Addons";
     @Nonnull
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "1.0.0";
     @Nonnull
     public static final String ENDERIO_VERSION = "@enderio_version@";
     @Nonnull
